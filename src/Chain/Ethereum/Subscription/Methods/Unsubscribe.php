cd33925e72818f2347f2005f2e32b91c82a16eca
echo <echo@huyg.com>
2023-03-16 09:51:57 +0800

    update Subscription and unSubscription

---
+++ src/Chain/Ethereum/Subscription/Methods/Unsubscribe.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Web3php\Chain\Ethereum\Subscription\Methods;
+
+use Web3php\Chain\Utils\JsonRpc\AbstractJsonRpc;
+
+class Unsubscribe extends AbstractJsonRpc
+{
+    protected string $method = "eth_unsubscribe";
+
+    public function __construct(string $subscribe)
+    {
+        $arguments[] = $subscribe;
+        $this->setArguments($arguments);
+    }
+}
